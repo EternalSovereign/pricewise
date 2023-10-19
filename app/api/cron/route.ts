682eeb568a329044ff219a5a88f19d224a9528f7
EternalSovereign <dipendrakumarbm5@gmail.com>
2023-10-19 19:49:54 +0530

    cron error 1

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -12,9 +12,9 @@ import Product from "@/lib/models/product.model";
 import { scrapeAmazonProduct } from "@/lib/scraper";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
+const maxDuration = 300; // This function can run for a maximum of 300 seconds
+const dynamic = "force-dynamic";
+const revalidate = 0;
 
 export async function GET(request: Request) {
   try {
